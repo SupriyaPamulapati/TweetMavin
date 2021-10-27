1640c320089dfb17b158ffcf44a23eeb9666b0ae
SupriyaPamulapati <pamulapati.supriya@khoros.com>
2021-10-27 16:51:06 +0530

    Worked on http status codes

--- src/main/java/UTwitter/resources/Resources.java
+++ src/main/java/UTwitter/resources/Resources.java
@@ -1,5 +1,7 @@
 package UTwitter.resources;
 
+import org.apache.commons.lang3.StringUtils;
+import org.eclipse.jetty.util.StringUtil;
 import twitter4j.Status;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
@@ -9,6 +11,8 @@ import twitter4j.conf.ConfigurationBuilder;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.core.Request;
+import javax.ws.rs.core.Response;
 import java.util.Date;
 import java.util.List;
 
@@ -17,48 +21,63 @@ import java.util.List;
     @Path("api/1.0/twitter")
 
     public class Resources {
+
+        class Request{
+            String  message;
+
+        }
+
         @GET
         @Path("/healthCheck")
-        public String healthCheck(){
-            return "ping Received"+ new Date();
+        public String healthCheck() {
+            return "ping Received" + new Date();
         }
 
         @POST
         @Path("/post")
-        public String tweetAgain() throws TwitterException {
+        public Response tweetpost(String post) {
+
             ConfigurationBuilder cb = new ConfigurationBuilder();
             cb.setDebugEnabled(true)
-                    .setOAuthConsumerKey("D4sBnyGzRjF8a78CW2aidR4zz")
-                    .setOAuthConsumerSecret("r9CyHHov8tuMK9sfC4CHWmNgzpijv6DGCUipf1SXcySxB9XluD")
-                    .setOAuthAccessToken("1450686116353376256-x1gEkshgxazcqGlkYwv2fDZAvIfW0s")
-                    .setOAuthAccessTokenSecret("Pse1CapF8eN5isvZhevLwosdnHFJrU6JZlSolOiilnB2k");
+                    .setOAuthConsumerKey("cnmrNRHyyRDr7tQEp29gyz6Ue")
+                    .setOAuthConsumerSecret("EiViCo8tBkbfxrZiUjqtWvDQmPTWTXsPvpzhIgEXFnzrVwCkd6")
+                    .setOAuthAccessToken("1450686116353376256-WHmrrz0iJhqvslfd3PKwdofj5an7bU")
+                    .setOAuthAccessTokenSecret("3H9hIiTTqa89WSoStyt1KOaBnEJKD7hBmVGIyoxXjtKAE");
             TwitterFactory tf = new TwitterFactory(cb.build());
             Twitter twitter = tf.getInstance();
-            twitter.updateStatus("again");
+            if(StringUtil.isEmpty(post)){
+                return Response.status(400,"please Enter a valid tweet").build();
+            }
+            else {
+                try {
+                    twitter.updateStatus(post);
+                }
+                catch (TwitterException e) {
+                    e.printStackTrace();
+                }
 
-            return "recieved a tweet";
+                return Response.status(200,"tweet posted").build();
+            }
         }
 
         @GET
         @Path("/get")
-        public String timeline() throws TwitterException {
+        public String tweetget() throws TwitterException {
+
             ConfigurationBuilder cb = new ConfigurationBuilder();
             cb.setDebugEnabled(true)
-                    .setOAuthConsumerKey("D4sBnyGzRjF8a78CW2aidR4zz")
-                    .setOAuthConsumerSecret("r9CyHHov8tuMK9sfC4CHWmNgzpijv6DGCUipf1SXcySxB9XluD")
-                    .setOAuthAccessToken("1450743367696994308-WM1cr2g13cLdLWQEnXo4QlKPc4Foi6")
-                    .setOAuthAccessTokenSecret("Pse1CapF8eN5isvZhevLwosdnHFJrU6JZlSolOiilnB2k");
+                    .setOAuthConsumerKey("cnmrNRHyyRDr7tQEp29gyz6Ue")
+                    .setOAuthConsumerSecret("EiViCo8tBkbfxrZiUjqtWvDQmPTWTXsPvpzhIgEXFnzrVwCkd6")
+                    .setOAuthAccessToken("1450686116353376256-WHmrrz0iJhqvslfd3PKwdofj5an7bU")
+                    .setOAuthAccessTokenSecret("3H9hIiTTqa89WSoStyt1KOaBnEJKD7hBmVGIyoxXjtKAE");
             TwitterFactory tf = new TwitterFactory(cb.build());
             Twitter twitter = tf.getInstance();
-
             List<Status> status = twitter.getHomeTimeline();
             for (Status st : status) {
-                System.out.println(st.getUser().getName() + "-------" + st.getText());
+               System.out.println(st.getUser().getName() + "-------" + st.getText());
 
             }
-            return "get a tweet";
+            return "Tweet posted";
         }
+    }
 
-
-
-}
