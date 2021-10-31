4b89193f8910b5e15fb6920cf11831f979b31242
SupriyaPamulapati <pamulapati.supriya@khoros.com>
2021-10-31 07:18:05 +0530

    Removed unused imports

--- src/main/java/UTwitter/MyTweet.java
+++ src/main/java/UTwitter/MyTweet.java
@@ -3,10 +3,7 @@ package UTwitter;
 import UTwitter.resources.Resources;
 import io.dropwizard.Application;
 import io.dropwizard.setup.Environment;
-import twitter4j.Twitter;
-import twitter4j.TwitterException;
-import twitter4j.TwitterFactory;
-import twitter4j.conf.ConfigurationBuilder;
+
 
 public class MyTweet extends Application<RestConfigurations> {
     public static void main(String[] args) throws Exception {
