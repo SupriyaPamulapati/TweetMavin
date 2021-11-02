77b45c03eda7d10f903ccbe9dc2a4f88cc7e97c5
SupriyaPamulapati <92838170+SupriyaPamulapati@users.noreply.github.com>
2021-11-02 15:12:42 +0530

    Update Resources.java

--- src/main/java/UTwitter/resources/Resources.java
+++ src/main/java/UTwitter/resources/Resources.java
@@ -5,7 +5,6 @@ import twitter4j.Status;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
-
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -14,14 +13,10 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.Date;
 import java.util.List;
-
-
     @Produces(MediaType.APPLICATION_JSON)
     @Path("api/1.0/twitter")
 
     public class Resources {
-
-
         @GET
         @Path("/healthCheck")
         public String healthCheck() {
@@ -30,10 +25,8 @@ import java.util.List;
 
         @POST
         @Path("/postTweets")
-        public Response tweetpost(Request request) {
-
+        public Response tweetPost(Request request) {
             Twitter twitter = TwitterFactory.getSingleton();
-
             String post=request.getMsg();
             if(StringUtil.isEmpty(post)){
                 return Response.status(400,"please Enter a valid tweet").build();
@@ -45,26 +38,29 @@ import java.util.List;
                 catch (TwitterException e) {
                     return Response.status(500,"tweet posted").build();
                 }
-
-                return Response.status(200,"tweet posted").build();
+               return Response.status(200,"tweet posted").build();
             }
         }
 
         @GET
         @Path("/getTweets")
-        public static String[] tweetget() throws TwitterException {
+        public static String[] tweetGet() throws TwitterException {
 
             Twitter twitter = TwitterFactory.getSingleton();
             List<Status> status = twitter.getHomeTimeline();
             int size = status.size();
             String str[] = new String[size];
             int i=0;
-            for (Status st : status) {
-
-               str[i]=st.getUser().getName() + "-------" + st.getText();
-               i++;
+             if(StringUtil.isEmpty(String.valueOf(str))) {
+                return Response.status(400, "please Enter a valid tweet").build();
             }
-            return str;
+            else {
+                for (Status st : status) {
+                    str[i]=st.getUser().getName() + "-------" + st.getText();
+                    i++;
+                   }
+                return str;
+                }
         }
     }
 
