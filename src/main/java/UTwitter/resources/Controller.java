17355b5320f48e9781c98d9c4c79cfa16bb7bd18
SupriyaPamulapati <pamulapati.supriya@khoros.com>
2021-11-11 12:51:08 +0530

    added loggers

--- src/main/java/UTwitter/resources/Controller.java
+++ src/main/java/UTwitter/resources/Controller.java
@@ -17,26 +17,33 @@ import javax.ws.rs.core.Response;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Produces(MediaType.APPLICATION_JSON)
 @Path("api/1.0/twitter")
 public class Controller {
+    private Logger log = LoggerFactory.getLogger(Controller.class);
 
     @GET
     @Path("/healthCheck")
     public String healthCheck() {
+       log.info("processing HealthCheck");
         return "ping Received" + new Date();
     }
 
     @GET
     @Path("/getTweets")
     public Response getTweets() {
+        log.info("Processing get request");
+        log.info("get");
         Twitter twitter = TwitterFactory.getSingleton();
         List<Status> status = null;
         try {
             status = twitter.getHomeTimeline();
         } catch (TwitterException e) {
             e.printStackTrace();
+            log.error("error",e);
         }
         int size = status.size();
         String[] str = new String[size];
@@ -48,6 +55,7 @@ public class Controller {
             for (Status st : status) {
                 str[i] = st.getUser().getName() + "-------" + st.getText();
                 i++;
+                log.info("Successfully retrieved Tweets");
             }
             return Response.ok(str).build();
         }
@@ -68,24 +76,31 @@ public class Controller {
     @POST
     @Path("/postTweets")
     public Response postTweets(MessageRequest request) {
+        log.info("Processing post request");
+        log.info("post");
         Twitter twitter = TwitterFactory.getSingleton();
         String post = request.getMsg();
         if (StringUtil.isEmpty(post)) {
+            log.error("please enter some valid tweet");
             return Response.status(400, "please Enter a valid tweet").build();
         } else {
             try {
 
                 Status status = twitter.updateStatus(post);
                 if (status.getText().equals(post)) {
+                    log.info("Tweet request Successful");
                     return Response.status(200, "the tweet is successfully posted").build();
                 } else {
+                    log.error("unable to process request");
                     return Response.status(500, "unable to process the request").build();
                 }
             } catch (TwitterException e) {
+                log.error("Tweet request is unsuccessful",e);
                 return Response.status(500, "the tweet is not successful").build();
             }
         }
     }
 
 }
+
 
