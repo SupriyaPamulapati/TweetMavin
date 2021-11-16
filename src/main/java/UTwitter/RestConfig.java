e039525ffb4a8e938f6a0ff144181e5053949345
SupriyaPamulapati <pamulapati.supriya@khoros.com>
2021-11-16 08:16:08 +0530

    changes on services and resources,

---
+++ src/main/java/UTwitter/RestConfig.java
@@ -0,0 +1,44 @@
+package UTwitter;
+
+import io.dropwizard.Configuration;
+import twitter4j.conf.ConfigurationBuilder;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+public class RestConfig extends Configuration {
+    String filepath="twitter4j.yml";
+    static String accessTokenSecret="";
+    static String consumerSecret="";
+     String consumerKey="";
+     String accessToken="";
+    Properties properties=new Properties();
+    FileInputStream fileInputStream;
+    {
+        try {
+            fileInputStream = new FileInputStream(filepath);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            properties.load(fileInputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        accessTokenSecret= properties.getProperty("accessTokenSecret");
+        consumerSecret= properties.getProperty("consumerSecret");
+        consumerKey= properties.getProperty("consumerKey");
+        accessToken= properties.getProperty("accessToken");
+    }
+    public ConfigurationBuilder configurationBuilder() {
+        ConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
+        configurationBuilder.setDebugEnabled(true)
+                .setOAuthConsumerKey(consumerKey)
+                .setOAuthConsumerSecret(consumerSecret)
+                .setOAuthAccessToken(accessToken)
+                .setOAuthAccessTokenSecret(accessTokenSecret);
+        return configurationBuilder;
+    }
+
+}
